17670eb8d9f27fcfbe577f08e208ca42d6b02ad0
Zain ul abdin <zainburfat112@gmail.com>
2021-10-11 17:59:50 +0500

    about file added

---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className="row">
+			<div className="col-sm-12">
+        <h2 className="alert alert-primary my-4">This is About Component</h2>
+        <p className="bg-info m-4 p-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit officiis, fugiat nostrum vel quia, sunt ut quasi accusamus necessitatibus quibusdam odit ullam enim error quas, ad nihil velit voluptatum. Doloribus!</p>
+      </div>
+    </div>
+  )
+}
